ed81b2fb4789988a4350cc3629e55c0460912294
danielyoon2013 <danielyoon2013@gmail.com>
2023-05-29 20:48:41 -0400

    Time: 49 ms, Memory: 16.8 MB - LeetHub

---
+++ generate-parentheses/generate-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        combinations = []
+        
+        def backtrack(path, idx, size_left_paren, size_right_paren):
+            print( path )
+            
+            if idx >= n and (size_left_paren > n or size_right_paren > n ):
+                return
+            
+            if idx == n*2:
+                combinations.append(path)
+            
+            if size_left_paren >= size_right_paren:
+                backtrack(path+"(", idx+1, size_left_paren+1, size_right_paren)
+                backtrack(path+")", idx+1, size_left_paren, size_right_paren+1)
+        
+        backtrack("",0,0,0)
+        return combinations
+            
+                
