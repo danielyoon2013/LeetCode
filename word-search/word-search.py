2d032bd0fb7bb29942b674296fe43672c462b301
danielyoon2013 <danielyoon2013@gmail.com>
2023-05-30 23:49:35 -0400

    Time: 9243 ms, Memory: 16.4 MB - LeetHub

---
+++ word-search/word-search.py
@@ -0,0 +1,29 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        n = len(board)
+        m = len(board[0])
+        
+        def backtrack(i, j, idx):
+            
+            if i < 0 or j  < 0 or i >= n or j >= m or board[i][j] != word[idx] or board[i][j] == "#" :
+                return False
+            
+            
+            if board[i][j] == word[idx] and idx == len(word)-1:
+                return True
+            
+            for move in [ (0,1), (1,0), (0,-1), (-1,0) ]:
+                temp = board[i][j]
+                board[i][j] = "#"
+                if backtrack(i+move[0], j+move[1], idx+1):
+                    return True
+                board[i][j] = temp
+                
+        for i in range(n):
+            for j in range(m):
+                if backtrack(i, j, 0):
+                    return True
+                
+        return False
+        
+
